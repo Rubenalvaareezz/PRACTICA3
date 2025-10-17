fb1ee0b32edfa3a67c0d1056b1adf25042f3a192
Rubenalvaareezz <rubalvrub@alum.us.es>
2025-10-17 10:32:23 +0200

    acabar

--- src/entrenos.py
+++ src/entrenos.py
@@ -31,12 +31,33 @@ def lee_entrenos(ruta_csv):
 
 
 def tipo_entreno(lista_entrenos):
-    tipos = []
-    for entrenos in lista_entrenos:
-        tipos.append(entrenos['tipo'])
-    return tipos
+    tipos = {entrenos[0] for entrenos in lista_entrenos}
+    return sorted(tipos)
+
+def duracion_entreno(lista_entrenos,d):
+    lista_mas_duracion_d = []
+    for entreno in lista_entrenos:
+        if entreno.duracion > d:
+            lista_mas_duracion_d.append(entreno)
+    return lista_mas_duracion_d
+
+def suma_calorias(lista_entrenos,f_inicial,f_final):
+    calorias = 0
+    lista_entrenamientos_fechas = []
+    for entreno in lista_entrenos:
+        if entreno[1] >= f_inicial and entreno[1]<= f_final:
+            lista_entrenamientos_fechas.append(entreno)
+            calorias += entreno[4]
+    return print(f"La suma de las calorias de todos los entrenos es:{calorias} calorias")
         
-    
+def mas_km(lista_entrenos):
+    lista_entrenamiento_mas_km = lista_entrenos[0]
+    for entreno in lista_entrenos:
+        if entreno[5] > lista_entrenamiento_mas_km[5]:
+            lista_entrenamiento_mas_km = entreno
+        else:
+            lista_entrenamiento_mas_km = lista_entrenamiento_mas_km
+    return print(f"El entrenamiento de mayor duracion es: {lista_entrenamiento_mas_km}")
     
     
     
--- src/entrenos_test.py
+++ src/entrenos_test.py
@@ -1,15 +1,35 @@
-from entrenos import lee_entrenos
-from entrenos import tipo_entreno
+from entrenos import *
+from datetime import datetime
 
 
-entrenamientos = lee_entrenos("data/entrenos.csv")
-for r in entrenamientos[:3  ]: 
-    print(r)
 
-for r in entrenamientos[-3: ]: 
-    print(r)
+if __name__ == "__main__":
+    entrenamientos = lee_entrenos("data/entrenos.csv")
+    for r in entrenamientos[:3  ]: 
+        print(r)
 
-tipo_entrenos = tipo_entreno("data/entrenos.csv")
-orden = tipo_entrenos(tipo_entreno)
-orden.sort()
-print(orden)
+    for r in entrenamientos[-3: ]: 
+        print(r)
+
+   
+    orden = tipo_entreno(entrenamientos)
+    print(f"Tipos de entreno: {orden}")
+
+    # d = int(input("Introduce una duración de entreno: "))
+    # tiempo_entrenos = duracion_entreno(entrenamientos,d)
+    # for r in tiempo_entrenos:
+    #     print(f"Los entrenos de mayor duracion que d son : {r}")
+
+    
+    # f_inicial = input("Introduzca una fecha inicial (dd/mm/YYYY): ")
+    # f_final = input("Introduzca una fecha final (dd/mm/YYYY): ")
+    # fecha_inical = datetime.strptime(f_inicial, "%d/%m/%Y").date()
+    # fecha_final = datetime.strptime(f_final, "%d/%m/%Y").date()   
+    # entrenos_fecha= suma_calorias(entrenamientos, fecha_inical, fecha_final)
+
+    mayor_duracion = mas_km(entrenamientos)
+    
+      
+                                   
+    
+   
